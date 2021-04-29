309c4c01767c4e28afa221cbf6d0bfe3f264d2cb
Jayden-Chiu <jaydenchiu@rogers.com>
2021-04-28 21:40:21 -0400

    added web storage and reactive forms to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxWebstorageModule } from 'ngx-webstorage';
 import { HomeComponent } from './home/home.component';
+import { PollTileComponent } from './shared/poll-tile/poll-tile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    SignupComponent,
+    HomeComponent,
+    PollTileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
