f44211da70044413d8ab32f950cf21ca7e9033d2
Jayden-Chiu <jaydenchiu@rogers.com>
2021-04-30 22:15:18 -0400

    created HTTP interceptor for attaching accessToken to every HTTP request

---
+++ src/app/token-interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { AuthService } from './auth/shared/auth.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(public authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authService.getToken()}`,
+      },
+    });
+    
+    return next.handle(req);
+  }
+}
