8406254d876de3a26cc329fc4b467759b5097604
Jayden-Chiu <jaydenchiu@rogers.com>
2021-05-03 21:00:33 -0400

    temporary test header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/shared/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.loggedIn.subscribe(
+      (data: boolean) => (this.isLoggedIn = data)
+    );
+      
+  }
+}
