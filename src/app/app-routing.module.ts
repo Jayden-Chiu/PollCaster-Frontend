581eae8e20ddb274093e271cea69e98d1b6ae85f
Jayden-Chiu <jaydenchiu@rogers.com>
2021-05-04 21:12:02 -0400

    add profile route to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './auth/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'profile/:userId', component: ProfileComponent },
 ];
 
 @NgModule({
