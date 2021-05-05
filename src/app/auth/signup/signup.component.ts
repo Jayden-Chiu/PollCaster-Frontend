99a471e1ce6752036af5992c2fca9d6d73f8330a
Jayden-Chiu <jaydenchiu@rogers.com>
2021-05-04 20:55:17 -0400

    created signup component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
 import { SignupRequestPayload } from './signup-request.payload';
 
@@ -11,12 +12,18 @@ import { SignupRequestPayload } from './signup-request.payload';
 export class SignupComponent implements OnInit {
   signupForm: FormGroup;
   signupRequestPayload: SignupRequestPayload;
+  errorMessage: string;
+  usernameEmpty: boolean;
+  passwordEmpty: boolean;
+  confirmPasswordMatch: boolean;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.signupRequestPayload = {
       username: '',
       password: '',
     };
+    this.errorMessage = '';
+    this.confirmPasswordMatch = true;
   }
 
   ngOnInit(): void {
@@ -28,19 +35,37 @@ export class SignupComponent implements OnInit {
   }
 
   signup() {
-    
     const password = this.signupForm.get('password').value;
     const confirmPassword = this.signupForm.get('confirmPassword').value;
-    
+
     this.signupRequestPayload.username = this.signupForm.get('username').value;
     this.signupRequestPayload.password = this.signupForm.get('password').value;
 
-    this.authService.signup(this.signupRequestPayload).subscribe((data) => {
-      console.log(data);
-    });
+    this.usernameEmpty = !this.signupForm.get('username').valid;
+    this.passwordEmpty = !this.signupForm.get('password').valid;
+    this.confirmPasswordMatch =
+      this.signupForm.get('password').value ===
+      this.signupForm.get('confirmPassword').value;
+
+    if (this.usernameEmpty || this.passwordEmpty) {
+      return;
+    }
+
+    if (!this.confirmPasswordMatch) {
+      return;
+    }
+
+    this.authService.signup(this.signupRequestPayload).subscribe(
+      (data) => {
+        console.log(data);
+        this.router.navigateByUrl('/login');
+      },
+      (error) => {
+        console.log(error.error.message);
+        this.errorMessage = error.error.message;
+      }
+    );
   }
 
-  checkPassword() {
-
-  }
+  checkPassword() {}
 }
