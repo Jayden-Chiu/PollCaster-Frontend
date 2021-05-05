694dab659df8ea12ab6801297053781493dd0558
Jayden-Chiu <jaydenchiu@rogers.com>
2021-05-05 01:00:57 -0400

    added poll creation to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './auth/profile/profile.component';
+import { AuthGuard } from './auth/auth.guard';
+import { CreatePollComponent } from './create-poll/create-poll.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile/:userId', component: ProfileComponent },
+  { path: 'create', component: CreatePollComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
